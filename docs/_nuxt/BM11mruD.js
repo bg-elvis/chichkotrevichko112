d059a838d9974e2b828ba29e4e052061496104cc
BG ELVIS <bgelves@proton.me>
2025-07-16 20:00:11 +0300

    Init

---
+++ docs/_nuxt/BM11mruD.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./k6nDU5VM.js";import"./_31TA2sh.js";export{m as default};
---
+++ docs/_nuxt/BzHoDI5A.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Bg_K7i2m.js";import"./Cb6M0QF3.js";import"./_31TA2sh.js";export{o as default};
